5154414f82650ae21b6131a494cef90fe378ee90
unknown <thtran1@697-334-Boromir.sf.k12.internal>
2015-08-25 11:35:48 -0700

    8/25 new functions

--- build-tmp/source/OriginalDesign.java
+++ build-tmp/source/OriginalDesign.java
@@ -14,14 +14,19 @@ import java.io.IOException;
 
 public class OriginalDesign extends PApplet {
 
+float x = 1;
+
 public void setup()
 {
 	size(400,400);
 	background(255, 183, 0);
+	noLoop();
 }
 public void draw()
 {
 	//ground
+	fill(255,183,0);
+	rect(0,0,400,200);
 	for (int a = 0; a < 1000; a = a + 1) {
     int x = a * 1;
     noStroke();
@@ -42,21 +47,22 @@ public void draw()
     fill(255,255,0);
     ellipse(-65+2*x, 150+1*x, 0.05f*x, 0.1f*x);
 	}
+	
 	//house
 	fill(255,255,255);
-	rect(150,75,200,100);
+	rect(150*x,75*x,200*x,100*x);
 	int b = 35;
 	while (b<200) {
 	    fill(0, 118, 173);
-	    rect(b+150,100,25,25);
+	    rect((b+150)*x,100*x,25*x,25*x);
 	    b+=100;
 	}
 	fill(0);
-	rect(235,130,25,45);
+	rect(235*x,130*x,25*x,45*x);
 	fill(0);
-	triangle(150,75,250,25,350,75);
+	triangle(150*x,75*x,250*x,25*x,350*x,75*x);
 	fill(0,0,0,100);
-	triangle(350,175,350,125,400,125);
+	triangle(350*x,175*x,350*x,125*x,400*x,125*x);
 	//fence
 	for (int a = 0; a < 50; a = a + 5) {
     int x = a * 1;
@@ -74,6 +80,16 @@ public void draw()
 	}
 }
 
+public void mousePressed() 
+{
+	x = x+0.01f;
+	redraw();
+}
+
+public void print()
+{
+	print("program started");
+}
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "OriginalDesign" };
     if (passedArgs != null) {
